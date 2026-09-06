13890334a03d2b417a896cec41881e9e30278d7d
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-09-06 21:17:53 +0800

    configure media and static file settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,7 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
+STATIC_ROOT = BASE_DIR/ "staticfiles"
+MEDIA_ROOT = BASE_DIR/ "mediafiles"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
