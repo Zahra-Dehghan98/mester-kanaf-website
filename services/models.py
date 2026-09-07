119595708ba20914096362aa6e5607d822de6cc5
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-09-07 21:00:26 +0800

    add Service model with admin and URL config, initial migration

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان")
+    description = models.TextField(verbose_name="توضیحات")
+    image = models.ImageField(upload_to="services/", blank=True, null=True, verbose_name="تصویر")
+    order = models.IntegerField(default=0, verbose_name="ترتیب")
+    is_active = models.BooleanField(default=True, verbose_name="فعال")
+
+    def __str__(self):
+        return self.title
