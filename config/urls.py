c73de049283d0300d484ae18aa691be4c4641635
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-09-07 22:37:11 +0800

    add services context to HomePageView in core app for single-page site structure

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from config.settings import base
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
-    path('services/', include("services.urls")),
 ]
+
+urlpatterns += static(base.STATIC_URL, document_root = base.STATIC_ROOT)
+urlpatterns += static(base.MEDIA_URL, document_root = base.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from services.models import Service
 
 class HomePageView(TemplateView):
     template_name = "core/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["services"] = Service.objects.filter(is_active = True).order_by("order")
+        return context
+    
 
